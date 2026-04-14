72fe744653c034ef224a78ebadbe8a78791216ae
Xixi Liu <xixiliu@Catherines-MacBook-Pro.local>
2026-04-14 13:13:47 -0500

    Finished draft version of favorites and worked on favorites functionality

--- favorites.js
+++ favorites.js
@@ -1,1 +1,24 @@
+function getFavorites() {
+    return JSON.parse(localStorage.getItem("favorites")) || [];
+}
 
+function saveFavorites(favorites) {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+}
+
+function addFavorite(recipe) {
+    let favorites = getFavorites();
+
+    const saved = favorites.some(fav => fav.id === recipe.id);
+
+    if (!saved) {
+        favorites.push(recipe);
+        saveFavorites(favorites);
+    }
+}
+
+function removeFavorite(id) {
+    let favorites = getFavorites();
+    favorites = favorites.filter(recipe => recipe.id !== id);
+    saveFavorites(favorites);
+}
--- planner.js
+++ planner.js
@@ -1,5 +1,4 @@
 // Popup recipe adder
-
 const popup = document.getElementById("recipe-popup");
 const closeBtn = document.getElementById("close-button");
 const addButtons = document.querySelectorAll(".add-button");
@@ -14,3 +13,30 @@ closeBtn.addEventListener("click", () => {
     popup.classList.add("hidden");
 });
 
+// Load favorited recipes into popup
+const popupRecipes = document.getElementById("popup-recipes");
+const favoriteRecipes = JSON.parse(localStorage.getItem("favorites")) || [];
+
+function loadPopupRecipes() {
+    for(let recipe of favoriteRecipes) {
+        const card = document.createElement("div");
+        card.classList.add("popup-card");
+
+        const img = document.createElement("img");
+        img.src = recipe.image;
+        img.alt = recipe.title;
+
+        const title = document.createElement("h5");
+        title.textContent = recipe.title;
+
+        const selectBtn = document.createElement("button");
+        selectBtn.classList.add("select-btn");
+        selectBtn.textContent = "Select";
+
+        card.appendChild(img);
+        card.appendChild(title);
+        card.appendChild(selectBtn);
+
+        popupRecipes.appendChild(card);
+    }
+}
