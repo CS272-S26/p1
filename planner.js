9429ae29e102bef6146849562fbe769904a732de
Xixi Liu <xixiliu@Catherines-MacBook-Pro.local>
2026-04-13 21:39:30 -0500

    Worked on Meal Planner page - Xixi

--- planner.js
+++ planner.js
@@ -1,1 +1,16 @@
+// Popup recipe adder
+
+const popup = document.getElementById("recipe-popup");
+const closeBtn = document.getElementById("close-button");
+const addButtons = document.querySelectorAll(".add-button");
+console.log(closeBtn);
+addButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        popup.classList.remove("hidden");
+    })
+})
+
+closeBtn.addEventListener("click", () => {
+    popup.classList.add("hidden");
+});
 
